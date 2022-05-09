046dbae4c6b685ec220965abc78918512a58f343
yamadayuya <47471299+y-u-y-a@users.noreply.github.com>
2022-05-09 17:07:08 +0900

    fix:

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,67 +1,13 @@
 import Link from 'next/link'
-import Image from 'next/image'
-// import AnchorLink from 'react-anchor-link-smooth-scroll'
 
-import Logo from '../images/logo.svg'
-import GroupLogo from '../images/groupLogo.gif'
-// import HeaderBgLine from '../images/headerBgLine.png'
-
-interface Props {
-  showProducts: boolean
-  setShowProducts: React.Dispatch<React.SetStateAction<boolean>>
-  isAnkerLink: boolean
-}
-
-export const Header = ({ showProducts, setShowProducts, isAnkerLink }: Props) => {
+export const Header = () => {
   return (
     <>
       <header>
-        <Logo className={{}} />
         <Link href="/">
-          <a target="_blank">
-            <Image src={GroupLogo} alt="" />
-          </a>
+          <a target="_blank">ヘッダータイトル</a>
         </Link>
       </header>
-      {/* <div className={styles.root}>
-        <header className={styles.header}>
-          <div className={styles['header-inner']}>
-            <Link href="/">
-              <a>
-                <Logo className={styles['logo']} />
-              </a>
-            </Link>
-            <Link href="https://www.sompo-hd.com/">
-              <a target="_blank" className={styles['group-logo']}>
-                <img src={groupLogo} alt="安心・安全・健康のテーマパーク" />
-              </a>
-            </Link>
-          </div>
-        </header>
-        <nav className={styles.nav}>
-          <ul>
-            <li className={showProducts ? styles['li-products--active'] : styles['li-products']} onClick={handleToggleShowProducts}>
-              <span>Products</span>
-            </li>
-            {isAnkerLink &&
-              links.map((link, index) => {
-                return (
-                  <li key={index}>
-                    <AnchorLink href={link.href}>{link.text}</AnchorLink>
-                  </li>
-                )
-              })}
-            {!isAnkerLink &&
-              links.map((link, index) => {
-                return (
-                  <li key={index}>
-                    <Link href={`${process.env.REACT_APP_URL}${link.href}`}>{link.text}</Link>
-                  </li>
-                )
-              })}
-          </ul>
-        </nav>
-      </div> */}
     </>
   )
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,35 +1,15 @@
 import React from 'react'
 import { AppProps } from 'next/app'
-// import "react-app-polyfill/ie11";
-// import { DefaultSeo } from "next-seo";
 
-// import "../App.scss";
-// import "../index.scss";
-// import og01 from "@/images/org/og01.jpg";
+import { Header } from '../components/Header'
+import { Footer } from '../components/Footer'
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <>
-      {/* <DefaultSeo
-        titleTemplate="%s | SOMPO Light Vortex"
-        defaultTitle="SOMPO Light Vortex"
-        twitter={{
-          cardType: "summary_large_image",
-          site: process.env.REACT_APP_URL,
-        }}
-        openGraph={{
-          type: "website",
-          locale: "ja_JP",
-          url: process.env.REACT_APP_URL,
-          site_name: `SOMPO Light Vortex`,
-          images: [
-            {
-              url: og01,
-            },
-          ],
-        }}
-      /> */}
+      <Header />
       <Component {...pageProps} />
+      <Footer />
     </>
   )
 }
